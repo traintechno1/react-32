7ee823efc2b18d0c9ed4cd8c7657efb9002073c5
traintechno1 <admin@Chaitalis-MacBook-Air.local>
2026-06-01 10:57:15 +0530

    Register functionality

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,13 +2,18 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from  "@hookform/resolvers/zod";
 import "./Register.css";
+import axios from "axios";
 
 const formSchema = z.object({
     first_name: z.string()
         .min(1, "First Name is required")
         .min(2, "First Name must be at least 2 characters")
         .regex(/^[A-Za-z]+$/, "Only Characters are allowed"),
-        last_name: z.string()
+    middle_name: z.string()
+        .min(1, "Middle Name is required")
+        .min(2, "Middle Name must be at least 2 characters")
+        .regex(/^[A-Za-z]+$/, "Only Characters are allowed"),
+    last_name: z.string()
         .min(1, "Last Name is required")
         .min(2, "Last Name must be at least 2 characters")
         .regex(/^[A-Za-z]+$/, "Only Characters are allowed"),
@@ -20,7 +25,17 @@ const formSchema = z.object({
     email: z.string()
         .min(1, "Email is required")
         .max(40, "Email should be less than 40 characters")
-        .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Enter valid email")
+        .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Enter valid email"),
+    gender: z.string()
+        .min(1, "Gender is required"),
+    aadhar: z.string()
+        .min(1, "AADHAR Number is required")
+        .regex(/^[2-9]{1}[0-9]{3}\s[0-9]{4}\s[0-9]{4}$/, "Please Enter valid AADHAR Number"),
+    pan_number: z.string()
+        .min(1, "PAN number is required")
+        .regex(/^[A-Z]{3}[PCHFATBLJG][A-Z]\d{4}[A-Z]$/, "Please Enter valid PAN Number"),
+    password: z.string()
+        .min(1, "Password is required")
 })
 
 export default function Register(){
@@ -29,14 +44,24 @@ export default function Register(){
         resolver: zodResolver(formSchema),
         defaultValues: {
             first_name: "",
+            middle_name: "",
             last_name: "",
+            email: "",
             mobile: "",
-            email: ""
+            gender: "",
+            aadhar: "",
+            pan_number: "",
+            password: ""
         }
     })
 
     function submit(data){
         console.log(data);
+        axios.post("http://localhost:4000/customer/register", data)
+            .then(res=>{
+                console.log(res);
+            })
+
     }
     return(
         <>
@@ -52,6 +77,16 @@ export default function Register(){
                 <br />
                 <p className="error-phrase">{errors?.first_name?.message}</p>
 
+                <label htmlFor="middle_name">Middle Name:</label>
+                <input 
+                    {...register("middle_name")}
+                    id="middle_name" 
+                    name="middle_name" 
+                    placeholder="Enter First Name" />
+
+                <br />
+                <p className="error-phrase">{errors?.middle_name?.message}</p>
+
                 <label htmlFor="last_name">Last Name:</label>
                 <input 
                     {...register("last_name")}
@@ -66,7 +101,6 @@ export default function Register(){
                 <input 
                     {...register("mobile")}
                     id="mobile" 
-                    name="mobile" 
                     placeholder="Enter Mobile Number" />
                 <br/>
                 <p className="error-phrase">{errors?.mobile?.message}</p>
@@ -74,11 +108,42 @@ export default function Register(){
                 <label htmlFor="email">Email:</label>
                 <input 
                     {...register("email")}
-                    id="email" 
-                    name="email" 
+                    id="email"  
                     placeholder="Enter email" />
                 <br/>
                 <p className="error-phrase">{errors?.email?.message}</p>
+                
+                <label>Gender:</label>
+                <input id="male" type="radio" value={"Male"} {...register("gender")} /> <label htmlFor="male">Male</label>
+                <input id="female" type="radio" value={"Female"} {...register("gender")} /> <label htmlFor="female">Female</label>
+                <p className="error-phrase">{errors?.gender?.message}</p>
+
+
+                <label htmlFor="aadhar">AADHAR Number:</label>
+                <input 
+                    {...register("aadhar")}
+                    id="aadhar"  
+                    placeholder="Enter aadhar number" />
+                <br/>
+                <p className="error-phrase">{errors?.aadhar?.message}</p>
+
+
+                <label htmlFor="pan">PAN Number:</label>
+                <input 
+                    {...register("pan_number")}
+                    id="pan"  
+                    placeholder="Enter PAN Number" />
+                <br/>
+                <p className="error-phrase">{errors?.pan_number?.message}</p>
+                
+                <label htmlFor="password">Password:</label>
+                <input 
+                    {...register("password")}
+                    id="password"  
+                    type="password"
+                    placeholder="Enter Password" />
+                <br/>
+                <p className="error-phrase">{errors?.password?.message}</p>
 
                 <button type="submit" onClick={submit}>submit</button>
                 <button onClick={()=> reset()}>Reset</button>
